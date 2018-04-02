fe54dae79eaafa59b6b9ec2b15ce59dfae87b433
Sadimfinity <36569035+Sadimfinity@users.noreply.github.com>
2018-04-02 15:13:49 -0500

    Update mascotas.sql

--- mascotas.sql
+++ mascotas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE articulo (id_articulo BIGINT NOT NULL, edad INT, especie VARCHAR(255), foto LONGBLOB NOT NULL, nombre VARCHAR(255) NOT NULL, precio DOUBLE NOT NULL, raza VARCHAR(255), PRIMARY KEY (id_articulo));
 
-CREATE TABLE articulo (id_articulo BIGINT NOT NULL, edad INT, especie VARCHAR(255), foto LONGBLOB NOT NULL, nombre VARCHAR(255) NOT NULL, precio DOUBLE NOT NULL, raza VARCHAR(255), PRIMARY KEY (id_articulo));
-
 CREATE TABLE venta (id_venta BIGINT NOT NULL, `ARTICULOSCOMPRADOS` LONGBLOB NOT NULL, PRIMARY KEY (id_venta));
 
 CREATE TABLE venta_articulo (`Venta_id_venta` BIGINT NOT NULL, articulos_id_articulo BIGINT NOT NULL, PRIMARY KEY (`Venta_id_venta`, articulos_id_articulo));
+
+CREATE TABLE factura (id_factura BIGINT NOT NULL, fecha DATE NOT NULL, nombre_comprador VARCHAR(255) NOT NULL, nombre_vendedor VARCHAR(255) NOT NULL, `VENTA_id_venta` BIGINT, PRIMARY KEY (id_factura));
