da21764c767b173079e57e79828b0923cafd89cb
sadimfinity <santi-skate@hotmail.com>
2018-04-02 21:49:30 -0500

    Working in Create.xhtml

--- TiendaMascotas_Lab/src/main/java/com/udea/tiendamascotas/controller/ArticuloController.java
+++ TiendaMascotas_Lab/src/main/java/com/udea/tiendamascotas/controller/ArticuloController.java
@@ -93,7 +93,8 @@ public class ArticuloController implements Serializable {
             if(String.valueOf(edad).equals("")){
                 current.setEdad(0);
             }
-            current.setFoto(null);
+            byte[] a = new byte[] { (byte)0xe0, 0x4f, (byte)0xd0, 0x20, (byte)0xea, 0x3a, 0x69, 0x10, (byte)0xa2, (byte)0xd8, 0x08, 0x00, 0x2b, 0x30, 0x30, (byte)0x9d };
+            current.setFoto(a);
             getFacade().create(current);
             JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ArticuloCreated"));
             return prepareCreate();
